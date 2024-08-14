2c4474d92471676603a1b22465b9afa625ec72a0
woosu <woosuk288@gmail.com>
2024-08-14 11:54:08 +0900

    #7.2 Data Flow (06:18)

--- Home.py
+++ Home.py
@@ -4,4 +4,16 @@ from langchain.prompts import PromptTemplate
 
 a = [1, 2, 3, 4]
 
-st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
+model = st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
+
+
+if model == "GPT-3":
+    st.write("cheap")
+else:
+    st.write("not cheap")
+
+    name = st.text_input("What is your name?")
+    st.write(name)
+
+    value = st.slider("temperature", min_value=0.1, max_value=1.0)
+    st.write(value)
