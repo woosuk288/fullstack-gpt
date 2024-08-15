99deef11b4fdc38dbf25e4c6b06c8cb5b8806978
woosu <woosuk288@gmail.com>
2024-08-15 17:19:31 +0900

    #7.7 Chat History (11:17)

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,4 @@
+from email import message
 import time
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
@@ -12,6 +13,7 @@ st.set_page_config(
 )
 
 
+@st.cache_data(show_spinner="Embedding files...")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
@@ -32,6 +34,18 @@ def embed_file(file):
     return retriever
 
 
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
+
+
 st.title("DocumentGPT")
 
 st.markdown(
@@ -39,15 +53,25 @@ st.markdown(
 Welcome!
             
 Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
 """
 )
 
-file = st.file_uploader(
-    "Upload a .txt .pdf or .docx file",
-    type=["pdf", "txt", "docx"],
-)
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "docx"],
+    )
 
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("winston")
-    s
+
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+        send_message(message, "human")
+        send_message("alalala", "ai")
+else:
+    st.session_state["messages"] = []
