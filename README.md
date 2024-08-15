8843ae3eaa2a1b047672803c618d508edf3e06c7
woosu <woosuk288@gmail.com>
2024-08-15 16:39:12 +0900

    #7.6 Uploading Documents (13:23)
    - 파일 업로드 관련 오류로 config.toml 설정 및 python-magic-bin 설치
    
    [참고 링크]
    https://blog.naver.com/kiddwannabe/223146804972
    https://stackoverflow.com/questions/76247540/loading-data-using-unstructuredurlloader-of-langchain-halts-with-tp-num-c-buf

---
+++ README.md
@@ -0,0 +1,1 @@
+### gitbash : source ./env/Scripts/Activate
--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,50 +1,53 @@
-import streamlit as st
 import time
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+import streamlit as st
+
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📃",
+)
 
 
-st.set_page_config(page_title="DocumentGPT", page_icon="📜")
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
 
 st.title("DocumentGPT")
 
+st.markdown(
+    """
+Welcome!
+            
+Use this chatbot to ask questions to an AI about your files!
+"""
+)
 
-# with st.chat_message("human"):
-#     st.write("Hellooooo")
+file = st.file_uploader(
+    "Upload a .txt .pdf or .docx file",
+    type=["pdf", "txt", "docx"],
+)
 
-# with st.chat_message("ai"):
-#     st.write("How are you")
-
-# with st.status("Embedding file...", expanded=True) as status:
-#     time.sleep(2)
-#     st.write("Getting the file")
-#     time.sleep(2)
-#     st.write("Embedding the file")
-#     time.sleep(2)
-#     st.write("Caching the file")
-#     status.update(label="Error", state="error")
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
-
-
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"message": message, "role": role})
-
-
-for message in st.session_state["messages"]:
-    send_message(message["message"], message["role"], save=False)
-
-# st.write(st.session_state["messages"])
-
-
-message = st.chat_input("Send a message to the ai")
-
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
-
-    with st.sidebar:
-        st.write(st.session_state)
+if file:
+    retriever = embed_file(file)
+    s = retriever.invoke("winston")
+    s
