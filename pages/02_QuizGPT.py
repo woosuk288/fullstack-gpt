e8c1187e69c46f709865ac4e2d118c3d1bb74504
woosu <woosuk288@gmail.com>
2024-08-18 15:57:59 +0900

    #9.3 Questions Prompt (09:52)

--- pages/02_QuizGPT.py
+++ pages/02_QuizGPT.py
@@ -4,6 +4,8 @@ from langchain.retrievers import WikipediaRetriever
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.chat_models import ChatOpenAI
+from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.prompts import ChatPromptTemplate
 
 st.set_page_config(
     page_title="QuizGPT",
@@ -12,7 +14,12 @@ st.set_page_config(
 
 st.title("QuizGPT")
 
-llm = ChatOpenAI(temperature=0.1, model="gpt-4o-mini")
+llm = ChatOpenAI(
+    temperature=0.1,
+    model="gpt-4o-mini",
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
+)
 
 
 @st.cache_data(show_spinner="Loading file...")
@@ -29,6 +36,10 @@ def split_file(file):
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     return docs
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
 
 
 with st.sidebar:
@@ -62,4 +73,44 @@ if not docs:
     """
     )
 else:
-    st.write(docs)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+    You are a helpful assistant that is role playing as a teacher.
+         
+    Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+    
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+         
+    Use (o) to signal the correct answer.
+         
+    Question examples:
+         
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+         
+    Your turn!
+         
+    Context: {context}
+""",
+            )
+        ]
+    )
+
+    chain = {"context": format_docs} | prompt | llm
+
+    start = st.button("Generate Quiz")
+
+    if start:
+        chain.invoke(docs)
